a62461f1ec31ed28d166c2a4788c81aa8c6ee9c2
Krish <krishsitikhu28@gmail.com>
2025-05-18 23:57:30 +0545

    responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,21 +3,26 @@ import { IoMdSend } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div className="px-20 py-10 bg-neutral-900 flex flex-col">
-      <div className=" flex text-neutral-200 gap-9 justify-around">
+    <div className="px-6 py-4 laptop:px-20 laptop:py-10 bg-neutral-900 flex flex-col">
+      <div className="grid grid-cols-2 laptop:grid-cols-4 text-neutral-200 gap-10 laptop:gap-20 mx-auto">
         <div className="flex flex-col gap-3">
-          <h1 className="text-3xl font-semibold">Exclusive</h1>
-          <h1>Subscribe</h1>
-          <h1>Get 10% off your first order</h1>
+          <h1 className="text-xl tablet:text-3xl font-semibold">Exclusive</h1>
+          <h1 className="text-sm :text-base">Subscribe</h1>
+          <h1 className="text-sm tablet:text-base">
+            Get 10% off your first order
+          </h1>
           <div className="relative">
-              <input className="border-1 px-4 py-2 pr-12" type="text" placeholder="Enter your email" />
-              <IoMdSend className="absolute right-3 top-2 cursor-pointer text-2xl"/>
+            <input
+              className="border-1 px-4 py-2 pr-8 tablet:pr-12 w-full text-sm tablet:text-base"
+              type="text"
+              placeholder="Enter your email"
+            />
+            <IoMdSend className="absolute right-3 top-2 cursor-pointer text-base tablet:text-2xl" />
           </div>
-          
         </div>
         <div className="space-y-2">
-          <h1 className="text-2xl font-semibold">Support</h1>
-          <ul className="space-y-2">
+          <h1 className="text-lg tablet:text-2xl font-semibold">Support</h1>
+          <ul className="space-y-2 text-sm tablet:text-base">
             <li>
               111 Bijay sarani, Dhaka,
               <br />
@@ -28,8 +33,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="space-y-1">
-          <h1 className="text-2xl font-semibold">Account</h1>
-          <ul className="space-y-2">
+          <h1 className="text-lg tablet:text-2xl font-semibold">Account</h1>
+          <ul className="space-y-2 text-sm tablet:text-base">
             <li>My Account</li>
             <li>Login / Register</li>
             <li>Cart</li>
@@ -38,16 +43,18 @@ const Footer = () => {
           </ul>
         </div>
         <div className="space-y-1">
-          <h1 className="text-2xl font-semibold">Quick Link</h1>
-          <ul className="space-y-2">
-              <li>Privacy</li>
-              <li>Terms Of USe</li>
-              <li>FAQ</li>
-              <li>Contact</li>
+          <h1 className="text-lg tablet:text-2xl font-semibold">Quick Link</h1>
+          <ul className="space-y-2 text-sm tablet:text-base">
+            <li>Privacy</li>
+            <li>Terms Of USe</li>
+            <li>FAQ</li>
+            <li>Contact</li>
           </ul>
         </div>
       </div>
-      <div className="text-neutral-600 mt-8 self-center">Copyright Rimel 2022. All right reserved</div>
+      <div className="text-neutral-600 mt-8 self-center">
+        Copyright Rimel 2022. All right reserved
+      </div>
     </div>
   );
 };
